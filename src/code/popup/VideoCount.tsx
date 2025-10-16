dc0219e2c323cbc10a796caef27f07650ef97484
arek <codingdayandnight@gmail.com>
2025-10-16 13:35:38 +0200

    add shown videos count

--- src/code/popup/VideoCount.tsx
+++ src/code/popup/VideoCount.tsx
@@ -4,13 +4,18 @@ interface Props {
 }
 
 export const VideoCount = ({ videoCount, hiddenVideoCount }: Props) => {
+  const shownVideoCount = videoCount - hiddenVideoCount;
+
   return (
     <section>
       <h4 className="youtube-magic-popup__video-count-heading">Video Count:</h4>
       <p className="youtube-magic-popup__video-count">
         {`Total:    ${videoCount}`}
       </p>
-      <p className="youtube-magic-popup__hidden-video-count">
+      <p className="youtube-magic-popup__video-count">
+        Shown: {shownVideoCount}
+      </p>
+      <p className="youtube-magic-popup__video-count">
         Hidden: {hiddenVideoCount}
       </p>
     </section>
