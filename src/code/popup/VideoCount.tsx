69b243d09f03f1075f7a158bed565a6bbe946dbb
arek <codingdayandnight@gmail.com>
2025-10-16 14:27:27 +0200

    refactor count

--- src/code/popup/VideoCount.tsx
+++ src/code/popup/VideoCount.tsx
@@ -9,15 +9,17 @@ export const VideoCount = ({ videoCount, hiddenVideoCount }: Props) => {
   return (
     <section>
       <h4 className="youtube-magic-popup__video-count-heading">Video Count:</h4>
-      <p className="youtube-magic-popup__video-count">
-        {`Total:    ${videoCount}`}
-      </p>
-      <p className="youtube-magic-popup__video-count">
-        Shown: {shownVideoCount}
-      </p>
-      <p className="youtube-magic-popup__video-count">
-        Hidden: {hiddenVideoCount}
-      </p>
+      <ul className="youtube-magic-popup__video-count-list">
+        <li className="youtube-magic-popup__video-count-item">
+          {`Total:    ${videoCount}`}
+        </li>
+        <li className="youtube-magic-popup__video-count-item">
+          Shown: {shownVideoCount}
+        </li>
+        <li className="youtube-magic-popup__video-count-item">
+          Hidden: {hiddenVideoCount}
+        </li>
+      </ul>
     </section>
   );
 };
