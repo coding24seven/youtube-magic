8b879d1fe37e501c7c329dc1f300b71dbc24b145
arek <codingdayandnight@gmail.com>
2025-10-24 17:12:18 +0200

    linted

--- src/code/popup/App.tsx
+++ src/code/popup/App.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import Options from "./Options";
-import { VideoCount } from "./VideoCount";
-import { FilterNames, ViewOptionNames } from "../types";
-import { useVideoCounts } from "./hooks/useVideoCounts";
-import { useFilters } from "./hooks/useFilters";
-import { useOptions } from "./hooks/useOptions";
-import { useExtensionIsEnabled } from "./hooks/useExtensionIsEnabled";
-import { useExtensionCanRun } from "./hooks/useExtensionCanRun";
+import { useState } from 'react';
+import Options from './Options';
+import { VideoCount } from './VideoCount';
+import { FilterNames, ViewOptionNames } from '../types';
+import { useVideoCounts } from './hooks/useVideoCounts';
+import { useFilters } from './hooks/useFilters';
+import { useOptions } from './hooks/useOptions';
+import { useExtensionIsEnabled } from './hooks/useExtensionIsEnabled';
+import { useExtensionCanRun } from './hooks/useExtensionCanRun';
 
 export interface SelectItemConfig {
   name: FilterNames | ViewOptionNames;
@@ -37,19 +37,19 @@ const App = () => {
     {
       name: Watched,
       checked: filterSelect.watched,
-      title: "Watched",
+      title: 'Watched',
     },
     {
       name: MembersOnly,
       checked: filterSelect.membersOnly,
-      title: "Members Only",
+      title: 'Members Only',
     },
   ];
   const optionsConfig: SelectItemConfig[] = [
     {
       name: VideoNumbersAreShown,
       checked: optionSelect.videoNumbersAreShown,
-      title: "Video Numbers",
+      title: 'Video Numbers',
     },
   ];
 
@@ -59,7 +59,7 @@ const App = () => {
         className="youtube-magic-popup__toggle-extension"
         onClick={handleExtensionToggle}
       >
-        {extensionIsEnabled ? "Disable YouTube Magic" : "Enable YouTube Magic"}
+        {extensionIsEnabled ? 'Disable YouTube Magic' : 'Enable YouTube Magic'}
       </button>
       {extensionIsEnabled && (
         <>
